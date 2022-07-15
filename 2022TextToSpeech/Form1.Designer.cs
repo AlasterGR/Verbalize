cd65fb7e556bb1b9baf0c0c10a2d5a9be8e8fe36
Giannis Ladas <giannis@diadrasis.gr>
2022-07-15 16:51:05 +0300

    Add project files.

---
+++ 2022TextToSpeech/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace _2022TextToSpeech
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(53, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 75);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ConvertSound";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(53, 190);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 75);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(53, 28);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 75);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "FindFile";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(193, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 324);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private OpenFileDialog openFileDialog1;
+        private Button button3;
+        private Label label1;
+    }
+}
---
+++ 2022TextToSpeech/Form1.cs
@@ -0,0 +1,82 @@
+namespace _2022TextToSpeech
+{
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+    using Microsoft.CognitiveServices.Speech;
+    using Microsoft.CognitiveServices.Speech.Audio;
+
+    public partial class Form1 : Form
+    {
+        private const string Key = "8ee4528192934fec97d9ac6b03685047";
+        private const string Location = "westeurope"; // Azure Speech Service Location
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.label1.Visible = false;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {               
+                this.label1.Text = openFileDialog1.FileName;
+                this.label1.Visible = true;
+            }           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textfile = this.label1.Text;
+            string text = System.IO.File.ReadAllText(textfile);
+            if (textfile.Contains(".txt")) 
+            {
+                Task task = SynthesizeAudioAsyncText(text, textfile.Replace(".txt", ".wav"));
+            } else if (textfile.Contains(".xml")) {
+                Task task = SynthesizeAudioAsyncXML(text, textfile.Replace(".xml", ".wav"));
+            }
+        }       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        static async Task SynthesizeAudioAsyncText(string txt, string soundfile)
+        {
+            var config = SpeechConfig.FromSubscription("8ee4528192934fec97d9ac6b03685047", "westeurope");           
+            config.SpeechSynthesisLanguage = "el-GR";
+            config.SpeechSynthesisVoiceName = "el-GR-AthinaNeural";
+
+            //using AudioConfig audioConfig = AudioConfig.FromWavFileOutput(soundfile);           
+            //using SpeechSynthesizer synthesizer = new SpeechSynthesizer(config, audioConfig);
+
+            using SpeechSynthesizer synthesizer = new SpeechSynthesizer(config);
+
+            await synthesizer.SpeakTextAsync(txt);           
+            MessageBox.Show("The text was read");
+        }
+
+        static async Task SynthesizeAudioAsyncXML(string txt, string soundfile)
+        {
+            var config = SpeechConfig.FromSubscription("8ee4528192934fec97d9ac6b03685047", "westeurope");
+            config.SpeechSynthesisLanguage = "en-US";
+            config.SpeechSynthesisVoiceName = "en-US-JennyNeural";
+
+            //using AudioConfig audioConfig = AudioConfig.FromWavFileOutput(soundfile);
+            //using SpeechSynthesizer synthesizer = new SpeechSynthesizer(config, audioConfig);
+
+            using SpeechSynthesizer synthesizer = new SpeechSynthesizer(config);
+          
+            await synthesizer.SpeakSsmlAsync(txt);
+            MessageBox.Show("The xml was read");
+        }
+
+
+
+
+    }
+}
